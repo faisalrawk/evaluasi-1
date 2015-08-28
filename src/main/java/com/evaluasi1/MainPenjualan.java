53bf3db156d1b27fe2a8fe451dfd83c30009e20e
unknown <faisalrawk@gmail.com>
2015-08-28 22:55:48 +0700

    faisal akbar

--- src/main/java/com/evaluasi1/MainPenjualan.java
+++ src/main/java/com/evaluasi1/MainPenjualan.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.evaluasi;
+package com.evaluasi1;
 
 import com.evaluasi1.domain.Penjualan;
 import com.evaluasi1.domain.User;
